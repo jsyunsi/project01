92118e13a3e9985861c35a568cd358727c86b4d5
zhangkai <sidepip@163.com>
2019-07-21 14:31:08 +0800

    这个就是一个基本的java应用

---
+++ module02/src/com/yunsi/HelloWorld2.java
@@ -0,0 +1,12 @@
+package com.yunsi;
+
+/**
+ * 作者：zz
+ * 日期：2019-07-21
+ * 2019/7/21
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("hhhhh");
+    }
+}
